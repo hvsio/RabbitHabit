f1afb810eb7cb1f186b90a17181059255d4d0bfe
MatthewAAU <44397196+MatthewAAU@users.noreply.github.com>
2019-04-12 13:17:19 +0200

    Update Registration.java

--- app/src/main/java/aau/itcom/rabbithabit/Registration.java
+++ app/src/main/java/aau/itcom/rabbithabit/Registration.java
@@ -1,13 +1,19 @@
 package aau.itcom.rabbithabit;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registration extends AppCompatActivity {
 
@@ -32,22 +38,54 @@ public class Registration extends AppCompatActivity {
         name = findViewById(R.id.name);
         registrationButton = findViewById(R.id.registerWithEmailButton3);
 
+//      Drawable error = getResources().getDrawable(R.drawable.baseline_error_outline_24);
+        email.setError("Please insert unique email");
+        password.setError("Please provide password");
+        name.setError("Please insert name");
+        passwordConf.setError("Passwords have to be identical");
+
+
     }
 
     public void registerUser(View view) {
+
         String emailText = email.getText().toString().replaceAll(" ","");
-        String passwordText = password.getText().toString().replaceAll(" ","");
-        String passwordConfText = passwordConf.getText().toString().replaceAll(" ","");
-        String nameText = name.getText().toString().replaceAll(" ","");
+        String passwordText = password.getText().toString();
+        String passwordConfText = passwordConf.getText().toString();
+        String nameText = name.getText().toString();
         if (emailText.equals("") || passwordText.equals("") || nameText.equals("")) {
             Toast.makeText(getApplicationContext(), "Please fill all the fields!", Toast.LENGTH_SHORT).show();
         }
         else if (!passwordConfText.equals(passwordText)) {
             Toast.makeText(getApplicationContext(), "Passwords do not match!", Toast.LENGTH_SHORT).show();
+
         }
         else {
-            Toast.makeText(getApplicationContext(), "User registered!", Toast.LENGTH_SHORT).show();
+            createAccount(emailText, passwordText);
         }
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        startActivity(MainPage.createNewIntent(getApplicationContext()));
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
     }
 
 }
