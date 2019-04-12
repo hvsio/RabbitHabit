129223bd98bc5d4e4b635b648a7e2b13370f1088
MatthewAAU <44397196+MatthewAAU@users.noreply.github.com>
2019-04-12 13:14:11 +0200

    Update MainPage.java

--- app/src/main/java/aau/itcom/rabbithabit/MainPage.java
+++ app/src/main/java/aau/itcom/rabbithabit/MainPage.java
@@ -2,8 +2,11 @@ package aau.itcom.rabbithabit;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.facebook.login.LoginManager;
@@ -15,15 +18,27 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     static Intent createNewIntent(Context context) {
         return new Intent(context, MainPage.class);
     }
 
-    public void logOutFromFrirebase(View view){
+    public void logOutFromFirebase(View view){
         LoginManager.getInstance().logOut();
         FirebaseAuth.getInstance().signOut();
         startActivity(LoginActivity.createNewIntent(getApplicationContext()));
     }
+
 }
