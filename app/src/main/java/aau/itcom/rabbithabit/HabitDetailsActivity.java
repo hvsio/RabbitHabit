83f2a3cd9c4f2b85f49ea45e0989a9a3d0d74452
Alicja <44125769+hvsio@users.noreply.github.com>
2019-04-14 23:18:55 +0200

    registration is initially done

---
+++ app/src/main/java/aau/itcom/rabbithabit/HabitDetailsActivity.java
@@ -0,0 +1,37 @@
+package aau.itcom.rabbithabit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+
+public class HabitDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.habitdetails);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        PointsGraphSeries<DataPoint> series = new PointsGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+        series.setShape(PointsGraphSeries.Shape.POINT);
+
+        graph.getViewport().setScrollable(true); // enables horizontal scrolling
+        graph.getViewport().setScrollableY(true); // enables vertical scrolling
+        graph.getViewport().setScalable(true); // enables horizontal zooming and scrolling
+        graph.getViewport().setScalableY(true); // enables vertical zooming and scrolling
+
+
+    }
+
+
+}
