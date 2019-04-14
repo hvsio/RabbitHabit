c318cd90e9d3c05c0d66186b771033bebc877cfa
FancyNameCreator <szymonso1009@gmail.com>
2019-04-14 15:17:30 +0200

    Firebase -> methods for adding both kinds of Habits, Stories, Photos are finished (should be working if we implement GUI) ~Simon

--- app/src/main/java/aau/itcom/rabbithabit/objects/Database.java
+++ app/src/main/java/aau/itcom/rabbithabit/objects/Database.java
@@ -5,11 +5,11 @@ import android.util.Log;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +17,7 @@ import java.util.Map;
 public class Database {
 
     // TODO : MAKE IT SINGLETON !!!
+    // TODO : UPDATE LOGS AFTER COPY & PASTE
 
     private static final String TAG = "DatabaseClass";
     FirebaseFirestore db;
@@ -65,17 +66,78 @@ public class Database {
     }
 
     public void addHabitPublished(HabitPublished habit){
+        Map<String, Object> map = new HashMap<>();
+        map.put("showCreator", habit.getShowCreator());
+        map.put("creator", habit.getCreator());
+        map.put("suggestedDuration", habit.getDuration());
+        map.put("numberOfLikes", 0);
+
+        db.collection("HabitsPublished").document(habit.getName())
+                .set(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Directory for Habits created!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error creating directory for Habits!", e);
+                    }
+                });
     }
 
     public HabitPublished getHabitPublishedByName(String name){
         return null;
     }
 
-    public void addPhoto(Photo photo){}
+    public void addPhoto(Photo photo, FirebaseUser user){
+        Map<String, Object> map = new HashMap<>();
+        map.put("URLinDATABASE", photo.getPhotoURLinDB());
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+
+        db.collection("Users").document(user.getUid()).collection("Photos").document(dateFormat.format(photo.getDate()))
+                .set(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Directory for Habits created!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error creating directory for Habits!", e);
+                    }
+                });
+    }
 
     public Photo getPhoto(Date date){return null;}
 
-    public void addStory (Story story){}
+    public void addStory (Story story, FirebaseUser user){
+        Map<String, Object> map = new HashMap<>();
+        map.put("storyContent", story.getTextContent());
+        map.put("mood", story.getMood());
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+
+        db.collection("Users").document(user.getUid()).collection("Stories").document(dateFormat.format(story.getDate()))
+                .set(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Directory for Habits created!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error creating directory for Habits!", e);
+                    }
+                });
+    }
 
     public Story getStory (Date date){return null; }
 
