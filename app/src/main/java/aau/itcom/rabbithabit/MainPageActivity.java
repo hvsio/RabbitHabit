b283f50aa70f5dfd9bc8ba2c0bee9a72ca9628df
MatthewAAU <44397196+MatthewAAU@users.noreply.github.com>
2019-04-15 22:23:51 +0200

    Update MainPageActivity.java

--- app/src/main/java/aau/itcom/rabbithabit/MainPageActivity.java
+++ app/src/main/java/aau/itcom/rabbithabit/MainPageActivity.java
@@ -8,36 +8,47 @@ import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.transition.TransitionManager;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainPageActivity extends AppCompatActivity {
+public class MainPageActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     FloatingActionButton fabHabit, fabStory, fabAdd, fabPhoto;
     CoordinatorLayout transitionsContainer;
     View viewBlurred;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_mainpage);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 <<<<<<< HEAD
         viewBlurred = findViewById(R.id.viewBlurred);
         transitionsContainer = findViewById(R.id.mainPageLayout);
+=======
+        fabAdd = findViewById(R.id.fabAdd);
+
+        transitionsContainer = findViewById(R.id.mainLayout);
+>>>>>>> ec0cfbb55d692bc2b377891182dd6a2d7160ee6a
         fabHabit = transitionsContainer.findViewById(R.id.fabHabit);
         fabStory = transitionsContainer.findViewById(R.id.fabStory);
         fabPhoto = transitionsContainer.findViewById(R.id.fabPhoto);
 
-        fabAdd = transitionsContainer.findViewById(R.id.fabAdd);
         fabAdd.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("RestrictedApi")
             @Override
@@ -63,6 +74,48 @@ public class MainPageActivity extends AppCompatActivity {
             }
         });
 
+<<<<<<< HEAD
+=======
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_calendar) {
+            // Handle the camera action
+        } else if (id == R.id.nav_profile) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_log_out) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+>>>>>>> ec0cfbb55d692bc2b377891182dd6a2d7160ee6a
     }
 
     static Intent createNewIntent(Context context) {
