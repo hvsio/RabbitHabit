29d20b4ab6d4328dcd2fd6e45e3e734ecd37eea3
MatthewAAU <44397196+MatthewAAU@users.noreply.github.com>
2019-04-15 12:32:27 +0200

    Update MainPageActivity.java

--- app/src/main/java/aau/itcom/rabbithabit/MainPageActivity.java
+++ app/src/main/java/aau/itcom/rabbithabit/MainPageActivity.java
@@ -1,18 +1,25 @@
 package aau.itcom.rabbithabit;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.transition.TransitionManager;
 import android.view.View;
 
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainPageActivity extends AppCompatActivity {
+
+    FloatingActionButton fabHabit, fabStory, fabAdd;
+    CoordinatorLayout transitionsContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +28,28 @@ public class MainPageActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        transitionsContainer = findViewById(R.id.mainPageLayout);
+        fabHabit = transitionsContainer.findViewById(R.id.fabHabit);
+        fabAdd = transitionsContainer.findViewById(R.id.fabAdd);
+
+        fabAdd = transitionsContainer.findViewById(R.id.fabAdd);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("RestrictedApi")
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                TransitionManager.beginDelayedTransition(transitionsContainer);
+                fabHabit.setVisibility(View.VISIBLE);
+                fabAdd.setVisibility(View.VISIBLE);
+            }
+        });
+
+        transitionsContainer.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(transitionsContainer);
+                fabHabit.setVisibility(View.GONE);
+                fabAdd.setVisibility(View.GONE);
             }
         });
     }
