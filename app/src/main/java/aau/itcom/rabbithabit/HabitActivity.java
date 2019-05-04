537d494686e663909aac881f9a9a2eed5af61e91
Alicja <44125769+hvsio@users.noreply.github.com>
2019-05-05 00:52:40 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/res/layout/activity_settings.xml
    #	app/src/main/res/layout/fragment_settings.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/aau/itcom/rabbithabit/HabitActivity.java
+++ app/src/main/java/aau/itcom/rabbithabit/HabitActivity.java
@@ -11,7 +11,6 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.timessquare.CalendarPickerView;
 
 import java.util.Calendar;
